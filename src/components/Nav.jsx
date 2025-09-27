889e37e63c1cc223043d99128665d71c0081bc7c
Aysen Tuna <150019846+AysT13@users.noreply.github.com>
2025-09-27 17:37:31 +0300

    Lesson 31A – Add auth components and pages

---
+++ src/components/Nav.jsx
@@ -0,0 +1,31 @@
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "../components/ui/navigation-menu";
+
+import { ModeToggle } from "../components/ModeToggle";
+
+export function Nav() {
+  return (
+    <div className="flex items-center gap-4">
+      <NavigationMenu>
+        {/* <NavigationMenuItem>
+              {/* <NavigationMenuLink href="/">Home</NavigationMenuLink>
+            </NavigationMenuItem> */}
+
+        <NavigationMenuList className="list-none flex items-center gap-4">
+          <NavigationMenuItem>
+            <NavigationMenuLink href="/login">Login</NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink href="/signup">Sign up</NavigationMenuLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+
+      <ModeToggle />
+    </div>
+  );
+}
