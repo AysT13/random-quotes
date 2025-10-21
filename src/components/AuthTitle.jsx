ad523073db8aeb3514ffefe3ff1eca1335197f24
Aysen Tuna <150019846+AysT13@users.noreply.github.com>
2025-10-21 12:32:38 +0300

    Delete src/components/AuthTitle.jsx

--- src/components/AuthTitle.jsx
+++
@@ -1,7 +0,0 @@
-export default function AuthTitle({ children }) {
-  return (
-    <h1 className="text-xl font-bold text-slate-800 dark:text-slate-300 text-center mb-3">
-      {children}
-    </h1>
-  );
-}
