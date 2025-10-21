3eb1e0c626150746d458596ce265af1fe436ffc5
Aysen Tuna <150019846+AysT13@users.noreply.github.com>
2025-10-21 12:32:51 +0300

    Delete src/components/Card.jsx

--- src/components/Card.jsx
+++
@@ -1,7 +0,0 @@
-export default function Card({ children }) {
-  return (
-    <div className="w-full md:w-xl bg-gray-200 dark:bg-gray-300 p-12 rounded-lg flex flex-col shadow-lg m-4 ">
-      {children}
-    </div>
-  );
-}
